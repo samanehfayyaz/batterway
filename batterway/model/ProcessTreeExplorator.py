b984bc9066986a4bb976a12afae9c1fe43902985
Simon van Lierde <simonvanlierde@gmail.com>
2024-10-09 20:15:28 +0200

    stuff

--- batterway/model/ProcessTreeExplorator.py
+++ batterway/model/ProcessTreeExplorator.py
@@ -1,25 +1,20 @@
 from typing import List
 
-from batterway.datamodel.generic.Process import Process
 from batterway.datamodel.generic.Product import Product, ProductArchetype, ProductInstance
 
 
-class ProcessTreeExplorator:
-    def __init__(self, process: Process):
-        self.processes = process
-        self.products: dict[str, Product] = dict()
-        self.product_archetypes: dict[str, List[ProductArchetype]] = dict()
-        self.product_instances: dict[str, ProductInstance] = dict()
+class WorldBank:
+    def __init__(self, products: list[Product], product_archetypes: list[ProductArchetype], product_instances: list[ProductInstance]):
+        self.products: dict[str, Product] = {Product.name: Product for Product in products}
+        self.product_archetypes: dict[str, ProductArchetype] = {ProductArchetype.product.name: ProductArchetype for ProductArchetype in product_archetypes}
+        self.product_instances: dict[str, ProductInstance] = {ProductInstance.product_archetype.product.name: ProductInstance for ProductArchetype in product_archetypes}
 
-    def build_leaf_inventory(self, product_instance: ProductInstance):
-        product_queue = [product_instance.get_absolute_bom()]
-        for product in product_instance.product_archtype.bom:
+    def get_product_instance_leaves(self, product_instance: ProductInstance):
+        for product in product_instance.bom:
             if product in self.product_archetypes:
                 product_archetype_bom = self.product_archetypes[product][0].bom
                 product_queue.append(product_archetype_bom)
 
 
-def build_leaf_inventory(product_instance: ProductInstance):
-    
-    product_instance.get_absolute_bom()
+
     
