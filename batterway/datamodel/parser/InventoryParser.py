73dfea2972aae3cc4b7a8729924c0a4e81b80bbf
leon ferrari <leon.ferrari@vub.be>
2024-10-10 10:06:04 +0200

    Update parsing and format

--- batterway/datamodel/parser/InventoryParser.py
+++ batterway/datamodel/parser/InventoryParser.py
@@ -19,30 +19,47 @@ class InventoryParser:
                 InventoryParser.__read_csv(file_name.joinpath("units.csv")).iterrows()
             )
         )
+        all_unit = {u.name: u for u in pydt_units_obj}
         pydt_products_parsed = list(map(
             lambda x: ProductPdt(
                 **x[1].to_dict()
                   |
                   {
-                      "BoM":[],"Quantity":QuantityPdt(**{"quantity":x[1].to_dict()["reference_quantity"],"unit":pydt_units_obj[0]})
+                      "reference_quantity": QuantityPdt(**{"quantity": x[1].to_dict()["reference_quantity"],
+                                                           "unit": all_unit[x[1].to_dict()["unit"]]})
                   }
             ),
-            InventoryParser.__read_csv(file_name.joinpath("products.csv")).fillna({"iri" : ""}).iterrows()
+            InventoryParser.__read_csv(file_name.joinpath("products.csv")).fillna({"iri": "", "BoM_id": ""}).iterrows()
         ))
         print(pydt_products_parsed)
         pydt_chemical_compounds = list(map(
-            lambda x: ChemicalCompoundPdt(**x[1].to_dict()),
-            InventoryParser.__read_csv(file_name.joinpath("chemical_compounds.csv")).iterrows()
+            lambda x: ChemicalCompoundPdt(
+                **x[1].to_dict()
+                  |
+                  {
+                      "reference_quantity": QuantityPdt(**{"quantity": x[1].to_dict()["reference_quantity"],
+                                                           "unit": all_unit[x[1].to_dict()["unit"]]})
+                  }
+            ),
+            InventoryParser.__read_csv(file_name.joinpath("chemical_compounds.csv")).fillna(
+                {"iri": "", "BoM_id": ""}).iterrows()
         ))
-        all_products = {p.name: p for p in pydt_products_parsed+pydt_chemical_compounds}
+
+        all_products = {p.name: p for p in pydt_products_parsed + pydt_chemical_compounds}
         bom_per_product = dict()
-        for product, df_bom_product in InventoryParser.__read_csv(file_name.joinpath("BoM.csv")).groupby("Product"):
-            bom_per_product[product] = BoMPdt(
-                product_quantities=[(all_products[row["Material"]], QuantityPdt(quantity=row["Quantity"], unit=pydt_units_obj[0]))
-                 for _, row in df_bom_product.iterrows()]
+        for BoMId, df_bom_product in InventoryParser.__read_csv(file_name.joinpath("BoM.csv")).groupby("BoMId"):
+            bom_per_product[BoMId] = BoMPdt(
+                **{
+                    "BoMId": BoMId,
+                    "product_quantities": {
+                        all_products[row["Material"]].name:
+                         QuantityPdt(quantity=row["Quantity"], unit=all_unit[row['Unit']])
+                        for _, row in df_bom_product.iterrows()
+                    }
+                }
 
             )
 
     @staticmethod
     def __read_csv(file_name: Path):
-        return pd.read_csv(file_name, sep=";",decimal=".")
+        return pd.read_csv(file_name, sep=";", decimal=".")
--- batterway/datamodel/parser/parsers.py
+++ batterway/datamodel/parser/parsers.py
@@ -4,16 +4,17 @@ class UnitPdt(pydantic.BaseModel):
     name: str
     iri:str
 class QuantityPdt(pydantic.BaseModel):
-    quantity: float
+    quantity: float|int
     unit:UnitPdt
 
 class ProductPdt(pydantic.BaseModel):
     name: str
     iri:str
     reference_quantity : QuantityPdt
-    BoM_id: str
+    BoM_id: str|None
 
 class BoMPdt(pydantic.BaseModel):
+    BoMId: str|None
     product_quantities : dict[str, QuantityPdt]
 class ChemicalCompoundPdt(ProductPdt):
     chemical_formulae: str
