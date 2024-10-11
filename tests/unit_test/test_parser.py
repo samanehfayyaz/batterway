dfdbef45d723d4977b6507237e7864efc9dd6b26
Simon van Lierde <simonvanlierde@gmail.com>
2024-10-11 11:12:15 +0200

    Some attempts at clarifying the cryptonomicon

--- tests/unit_test/test_parser.py
+++ tests/unit_test/test_parser.py
@@ -3,19 +3,10 @@ from pathlib import Path
 from batterway.datamodel.parser.Inventory import Inventory
 
 
-# Find path of the file containing this code
-
-file_path = 
-
-
-
 def test_parser():
-    new_inventory = Inventory.create_from_file(Path(__file__).parent.parent / "data/dataframes/")
+    new_inventory = Inventory.create_from_file(Path(__file__).parent.parent.parent / "data/dataframes/")
     r_process = new_inventory.get_process("route1")
-    r_process.update_fixed_input_lci(
-        {
-            "Battery_NMC442":1.0
-        }
-    )
+    r_process.update_fixed_input_lci({"Battery_NMC442": 1.0})
+
 
 test_parser()
